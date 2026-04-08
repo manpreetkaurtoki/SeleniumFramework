7f4fe1ad491f3224d23a9f39fffb9885f8a72455
Manpreet <manpreetkaurtoki@gmail.com>
2026-04-08 17:53:23 -0400

    PageObjectModel dropdown internetherokuapp

--- src/main/java/pageclasses/WelcomeToInternet.java
+++ src/main/java/pageclasses/WelcomeToInternet.java
@@ -9,13 +9,19 @@ public class WelcomeToInternet {
 	WebDriver driver;
 	WebDriverWait wait;
 	By formAuthlink = By.xpath("//a[text()='Form Authentication']");
+	By dropdownlink = By.xpath("//a[text()='Dropdown']");
+	
 
 	public WelcomeToInternet(WebDriver driver, WebDriverWait wait) {
 		this.driver = driver;
 		this.wait = wait;
 	}
 
-	public void ClickFormAuth() {
+	public void clickFormAuth() {
 		wait.until(ExpectedConditions.visibilityOfElementLocated(formAuthlink)).click();
+	}
+	public void clickDropdown()
+	{
+		wait.until(ExpectedConditions.visibilityOfElementLocated(dropdownlink)).click();
 	}
 }
--- src/test/java/internetherokuapptests/FormAuthTest.java
+++ src/test/java/internetherokuapptests/FormAuthTest.java
@@ -3,6 +3,7 @@ package internetherokuapptests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import pageclasses.DropDownPage;
 import pageclasses.LoginPage;
 import pageclasses.SecureAreaPage;
 import pageclasses.WelcomeToInternet;
@@ -12,16 +13,28 @@ public class FormAuthTest extends BaseTest {
 	@Test
 	public void loginSuccesTest() {
 		WelcomeToInternet entryObj = new WelcomeToInternet(driver, wait);
-		entryObj.ClickFormAuth();
+		entryObj.clickFormAuth();
 
 		LoginPage loginObj = new LoginPage(driver, wait);
 		loginObj.enterUsername();
 		loginObj.enterPassword();
 		loginObj.clickonSubmit();
-		
-		SecureAreaPage headObj= new SecureAreaPage(driver, wait);
+
+		SecureAreaPage headObj = new SecureAreaPage(driver, wait);
 		String compareHeading = headObj.getSuccessMessage();
 		Assert.assertTrue(compareHeading.contains("Secure Area"));
+		System.out.println("Login successfully");
+
+	}
+
+	@Test
+	public void dropdownfunc() {
+		WelcomeToInternet entryObj = new WelcomeToInternet(driver, wait);
+		entryObj.clickDropdown();
+		
+		DropDownPage dropdownObj = new DropDownPage(driver, wait);
+		dropdownObj.getDropDownItem(2);
+	
 
 	}
 }
